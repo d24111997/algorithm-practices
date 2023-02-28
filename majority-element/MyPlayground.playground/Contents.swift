b56d79abf0ffeb032f0e24cb7281237acbda8e8c
d24111997 <pqdung01654178588@gmail.com>
2023-02-28 23:08:55 +0700

    Majority Element

---
+++ majority-element/MyPlayground.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+/*
+ Given an array nums of size n, return the majority element.
+
+ The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+  
+
+ Example 1:
+
+ Input: nums = [3,2,3]
+ Output: 3
+ Example 2:
+
+ Input: nums = [2,2,1,1,1,2,2,3,4,2,2]
+ Output: 2
+  
+
+ Constraints:
+
+ n == nums.length
+ 1 <= n <= 5 * 104
+ -109 <= nums[i] <= 109
+  
+
+ Follow-up: Could you solve the problem in linear time and in O(1) space?
+ */
+
+class Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        var candidate = 0, vote = 0
+        nums.forEach { n in
+            if vote == 0 { candidate = n }
+            vote += n == candidate ? 1 : -1
+        }
+        return candidate
+    }
+}
+
+// P/S: Give up after 24 mins
